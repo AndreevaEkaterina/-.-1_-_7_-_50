18d8356317e17d08fae0516ab6946b46240032a3
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-23 15:39:20 +0800

    решила задачу 50

--- Program.cs
+++ Program.cs
@@ -7,3 +7,52 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+void Address(int[,] inArray, int Num)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] == Num)
+            {
+                Console.WriteLine($"Число расположено в {i} строке, {j} столбце\t ");
+                return;
+            }
+        }
+    }
+    Console.WriteLine("Число отвутствует в массиве.");
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число проверки: ");
+int checkNum = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(row, columns, 0, 10);
+PrintArray(array);
+Address(array, checkNum);
